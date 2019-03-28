e8f08f8d173fff734f284a32cb954e3ec9bdf60f
Edward Kurniady <t-edward.kurniady@ovo.id>
2019-03-28 12:21:10 +0700

    change port

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const Promise = require('bluebird');
 
 const server = Hapi.server({
     host: '0.0.0.0',
-    port:1311,
+    port: 443,
 });
 
 const req = request.defaults({
@@ -89,7 +89,7 @@ server.route(
 server.route(
   {
     method: 'GET',
-    path: '/',
+    path: '/', 
     handler : async (request, h) => {
       return 'TESTING';
       // return await postLB(request.payload) === 200 ? 'success!' : 'failed lol';
